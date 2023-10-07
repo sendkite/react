55943024c8f2a0e85cd59d88fe9f8adbaf4ce882
sendkite <sendkite1@gmail.com>
2023-10-07 20:49:35 +0900

    feat add game history

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import Board from "./App";
+import Game from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Board />
+    <Game />
   </StrictMode>
 );
